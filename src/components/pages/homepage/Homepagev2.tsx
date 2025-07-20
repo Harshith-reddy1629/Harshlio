36355c7738310eeeadd16d0de6c66e47b3a36064
harshith-reddy1629 <reddybharshith3@gmail.com>
2025-07-20 18:49:04 +0530

    ädd: emailjs resume

--- src/components/pages/homepage/Homepagev2.tsx
+++ src/components/pages/homepage/Homepagev2.tsx
@@ -9,7 +9,7 @@ import ConnectSection from "./Contact/ConnectSection";
 export default function Homev2() {
   return (
     <>
-      <div className="animate-slider overflow-hidden flex items-center gap-6 sm:bg-[linear-gradient(90deg,#0000_1%_,var(--background)_1%_99%_,#0000_99%),radial-gradient(65%_300%_at_0%_calc(100%-var(--y)),#fff9,transparent_10%),radial-gradient(65%_300%_at_100%_var(--y),#fff9,transparent_10%)] bg-size-[_5%_5%,cover,cover] pt-22 font-[family-name:var(--font-geist-mono)] *:flex-[1_1_50%] max-md:flex-col max-md:items-center max-md:gap-20 md:py-32 md:pt-42">
+      <div className="animate-slider flex items-center gap-6 overflow-hidden bg-size-[_5%_5%,cover,cover] pt-22 font-[family-name:var(--font-geist-mono)] *:flex-[1_1_50%] max-md:flex-col max-md:items-center max-md:gap-20 md:py-32 md:pt-42">
         <DeveloperProfile />
         <DeveloperBio />
       </div>
