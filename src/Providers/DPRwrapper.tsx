9fc052ae4ddde427c6914360c7beca17a59191be
harshith-reddy1629 <reddybharshith3@gmail.com>
2025-07-29 23:53:32 +0530

    add: updates

--- src/Providers/DPRwrapper.tsx
+++ src/Providers/DPRwrapper.tsx
@@ -12,24 +12,6 @@ export default function DPRwrapper() {
   const { triggerAction } = useTriggerAction();
 
   useEffect(() => {
-    const f = () => {
-      const dpr = window.devicePixelRatio || 1;
-      if (
-        /**window?.devicePixelRatio * innerWidth >= 768 &&*/
-        innerWidth >= 768 &&
-        dpr <= 1.25
-      ) {
-        document.body.style.zoom = `${1.25 / dpr}`;
-      }
-    };
-    f();
-    window.addEventListener("resize", f);
-    return () => {
-      window.removeEventListener("resize", f);
-    };
-  }, []);
-
-  useEffect(() => {
     triggerAction?.();
   }, [triggerAction]);
 
